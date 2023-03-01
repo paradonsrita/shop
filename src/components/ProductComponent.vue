dc087a8dc25d5c25841c7083fbccbb89c21db936
paradonsrita <paradonsrita2003@gmail.com>
2023-03-01 15:54:15 +0700

    0.1

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,103 @@
+<template>
+    <li>
+      <img :src="picture" ref="imageURL"/> 
+    <h1> {{ name }} </h1>
+    <button @click="showData(id)">{{ isvisible ? "hit" : "show" }}</button>&nbsp;
+    
+
+
+
+    <div v-show="isvisible">
+      <div class="boxShow">
+        <hr>
+
+        <hr>
+        <h4>{{ detail }}</h4>
+        <hr>
+        <h2>ราคา: {{ price }} บาท</h2>
+      </div>
+    </div>
+  </li>
+  </template>
+  
+  <script>
+
+  export default {
+      name:"ProductComponent",
+ 
+      methods: {
+        showData(id) {
+          this.$emit("showEMP", id)
+        },
+      },
+
+      props:{
+        id:{
+          type:Number
+        },
+        name: {
+            type:String,
+            required:true
+        },
+        detail:{
+            type:String,
+        },
+        type:{
+          type:String,
+          required:true
+        },
+        price:{
+          type:Number,
+          required:true
+        },
+        picture:{
+          type:String,
+          required:true
+
+        },
+        color:{
+          type:String,
+          required:true
+        },
+        isvisible:{
+          type:Boolean
+        }
+      }
+  }
+  </script>
+  
+  <style scoped>
+  
+    li{
+      margin:1rem;
+      font-size: 0.75rem;
+      font-weight: bold;
+      background: #8ddba4;
+      padding: 0.5rem;
+      color: #1f1f1f;
+      border-radius: 25px;
+
+      box-shadow:2px 4px 1px #aaa;
+    
+    }
+    button{
+      font:inherit;
+      cursor: pointer;
+      border:1px solid #ff0077;
+      background-image: linear-gradient(180deg, red, yellow);
+      color: rgb(9, 9, 9);
+      padding: 0.05rem 1rem;
+      box-shadow:1px 1px 2px rgba(0, 0, 0, 0.26)
+    }
+    .boxShow{
+      margin: 15px;
+    }
+    img{
+      margin: 10px;
+      width: 100px;
+      height: 100px;
+    }
+    .boxShow h4{
+      text-align: left;
+    }
+  </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <h1>Shop</h1>
+  </header>
+  <section class="employee-content">
+    <h2>ข้อมูลสินค้า</h2>
+    
+    <ListProduct />
+  
+  </section>
+</template>
+
+<script>
+import ListProduct from '../components/ListProduct.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    ListProduct
+  }
+}
+</script>
+
+
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+header {
+  margin: 3rem;
+  box-shadow: 0 2px 8px black;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: orange;
+  color: green;
+  text-align: center;
+}
+.employee-content {
+  box-shadow: 0 2px 8px black;
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+}
+.employee-content h2 {
+  font-size: 1.25rem;
+  border-bottom: 4px solid #ccc;
+  font-weight: bold;
+  color: purple;
+  margin: 0 0 0.5rem 0;
+}
+
+
+</style>
